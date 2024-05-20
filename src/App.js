5504a39591547ae6214ed68d0a722b0108abc30f
Anaglebia <bia22.sp@icloud.com>
2024-05-20 19:32:24 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './Components/Header';
+
+import Header from "./Components/Header/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+     <h1>Loja</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// cor = #ebebeb
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+
+
+function Header() {
+  return (
+    <header>
+       <nav class="navbar bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand">MKS Systemas</a>
+            </div>
+            <div>
+                <a class="btn btn-primary" href="#" role="button">Link</a>
+                <button type="button" class="btn btn-outline-light">Light</button>
+            </div>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
